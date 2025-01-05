e34395bb557e1bf3207a80c5f722e2f6ab5d5425
Arsh Jameel <arshjameel.24@protonmail.com>
2025-01-05 01:59:07 -0500

    v0.2 - added rough frontend and backend mvp 1. added working buttons for erasing, clearing and positive/negative conversion. 2. added some logic to handle equations and number displays. 3. logic is maintained by many flags. this resulted in code that is far from clean or maintainable, but it acts as a proof of concept. refining needed later.

--- script.js
+++ script.js
@@ -1,3 +1,128 @@
+const keypad = document.querySelector('.keypad');
+const topElement = document.querySelector('.top');
+const bottomElement = document.querySelector('.bottom');
+
+let firstNumber = '0';
+let secondNumber = '0';
+let currentOperation = '';
+let isFirstNumber = true;
+let isSecondNumber = false;
+let equalOperation = false;
+let isOperation = true;
+
+const mathOperations = ['/', '*', '-', '+', '^'];
+
+keypad.addEventListener('click', (e) => {
+    const buttonValue = e.target.textContent.trim();
+    
+    if (!isNaN(buttonValue) || buttonValue === '.') {
+        if (isFirstNumber && !isSecondNumber && isOperation && !equalOperation) {
+            if (firstNumber === '0') {
+                firstNumber  = buttonValue;
+            } else {
+                if (buttonValue === '.' && firstNumber.includes('.')) {
+                    return;
+                }
+                firstNumber += buttonValue;
+            }
+            bottomElement.textContent = firstNumber;
+            topElement.textContent = firstNumber;
+            isFirstNumber = true;
+            isOperation = true;
+            isSecondNumber = false;
+        }
+        
+        if (!isFirstNumber && isSecondNumber && isOperation) {
+            if (secondNumber === '0') {
+                secondNumber = buttonValue
+            } else {
+                if (buttonValue === '.' && secondNumber.includes('.')) {
+                    return;
+                }
+                secondNumber += buttonValue;
+            }
+            bottomElement.textContent = secondNumber;
+            topElement.textContent = `${firstNumber}${currentOperation}${secondNumber}`;
+            isFirstNumber = false;
+            isOperation = false;
+            isSecondNumber = true;
+            equalOperation = true;
+        }
+    } else {
+        if (mathOperations.includes(buttonValue)) {
+            if (!isOperation && isSecondNumber && currentOperation) {
+                if (firstNumber === '') {
+                    firstNumber = '0';
+                }
+                firstNumber = operate(currentOperation, parseFloat(firstNumber), parseFloat(secondNumber));
+                equalOperation = false;
+                isOperation = true;
+            }
+
+            if (isOperation) {
+                currentOperation = buttonValue;
+                isFirstNumber = false;
+                isOperation = true;
+                isSecondNumber = true;
+                secondNumber = '';
+                topElement.textContent = `${firstNumber}${currentOperation}`;
+                bottomElement.textContent = currentOperation;
+            }
+        }
+        
+        if (buttonValue === '=' && equalOperation) {
+            topElement.textContent = '';
+            firstNumber = operate(currentOperation, parseFloat(firstNumber), parseFloat(secondNumber));
+            bottomElement.textContent = firstNumber;
+            
+            isFirstNumber = false;
+            isOperation = true;
+            isSecondNumber = false;
+            equalOperation = false;
+        }
+
+        if (buttonValue === 'AC') {
+            firstNumber = '';
+            secondNumber = '';
+            topElement.textContent = '';
+            bottomElement.textContent = '0';
+            
+            isFirstNumber = true;
+            isOperation = true;
+            isSecondNumber = false;
+        }
+
+        if (buttonValue === 'C') {
+            let temp = topElement.textContent;
+            temp = temp.slice(0, -1);
+            topElement.textContent = temp;
+            if (temp.includes(currentOperation)) {
+                let operationIndex = temp.indexOf(`${currentOperation}`);
+                firstNumber = temp.slice(0, operationIndex+1).trim();
+                secondNumber = temp.slice(operationIndex, temp.length).trim();
+            } else {
+                firstNumber = temp.slice(0, temp.length).trim();
+                secondNumber = '0';
+            }
+        }
+
+        if (buttonValue === '+/-') {
+            let temp = bottomElement.textContent;
+            if (!isOperation) {
+                return;
+            }
+            if (temp.includes('-')) {
+                firstNumber = temp.slice(1, temp.length);
+                bottomElement.textContent = firstNumber;
+            } else {
+                firstNumber = `-${temp}`;
+                bottomElement.textContent = firstNumber;
+            }
+        }
+    }
+
+})
+
 const add = function (a, b) {
     return a + b;
   };
@@ -20,32 +145,22 @@ const divide = function (a, b) {
     return a / b;
 }
 
-const percentage = function (a) {
-    return a / 100;
-}
-
-const sqroot = function (a) {
-    return a ** (0.5);
-}
-
 const exponentiate = function (a, b) {
     return a ** b;
 }
 
-const pi = () => {
-    return 3.141592653589793;
-}
-
 const operate = function (operator, a, b) {
     switch(operator) {
-        case add:
+        case '+':
             return add(a, b);
-        case subtract:
+        case '-':
             return subtract(a, b);
-        case multiply:
+        case '*':
             return multiply(a, b);
-        case divide:
+        case '/':
             return divide(a, b);
+        case '^':
+            return exponentiate(a, b);
     }
 }
 
@@ -54,9 +169,6 @@ module.exports = {
     subtract,
     multiply,
     divide,
-    percentage,
-    sqroot,
     exponentiate,
-    pi,
     operate,
   };
